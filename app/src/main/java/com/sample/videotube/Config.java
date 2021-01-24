c4364794c41c7ada6d30efb16954792ed9778d37
ambishiva <shivasinghal123@gmail.com>
2021-01-24 08:09:16 +0530

    you tube player added

---
+++ app/src/main/java/com/sample/videotube/Config.java
@@ -0,0 +1,14 @@
+package com.sample.videotube;
+
+/**
+ * Created by echessa on 1/13/17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyA66QfzfH-NG317onDBe3O6lnx6OBRxx94";
+
+}
